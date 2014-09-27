e63a68b7e3f0d347141db362b53de11c994f96ad
starzou <starzou@126.com>
2014-09-27 18:21:45 +0800

    封装异常处理

---
+++ src/main/java/com/eliteams/quick4j/core/entity/ServiceException.java
@@ -0,0 +1,11 @@
+package com.eliteams.quick4j.core.entity;
+
+/**
+ * @author StarZou
+ * @class ServiceException
+ * @description 封装业务层发生的异常
+ * @since 2014-09-27 18:09
+ */
+public class ServiceException extends UserException {
+
+}
