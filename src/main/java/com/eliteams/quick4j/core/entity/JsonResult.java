f618f57ab3a80a5163c6231730d4a1df1d1cd80b
starzou <starzou@126.com>
2014-09-27 17:19:21 +0800

    封装错误处理对象

--- src/main/java/com/eliteams/quick4j/core/entity/JsonResult.java
+++ src/main/java/com/eliteams/quick4j/core/entity/JsonResult.java
@@ -1,30 +1,23 @@
 package com.eliteams.quick4j.core.entity;
 
-import java.io.Serializable;
 
 /**
- * Response JsonResult for RESTful <br>
- * 封装 返回Json 格式数据
- * 
+ * Response JSONResult for RESTful
+ * 封装返回JSON格式的数据
+ *
  * @author StarZou
  * @since 2014年5月26日 上午10:51:46
- **/
+ */
 
-public class JsonResult<T> implements Serializable {
-    private static final long serialVersionUID = -4699713095477151086L;
+public class JSONResult<T> extends Result {
+
+    private static final long serialVersionUID = 7880907731807860636L;
 
     /**
      * 数据
      */
     private T data;
-    /**
-     * 信息
-     */
-    private String message;
-    /**
-     * 是否成功
-     */
-    private boolean success;
+
 
     public T getData() {
         return data;
@@ -34,40 +27,42 @@ public class JsonResult<T> implements Serializable {
         this.data = data;
     }
 
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public boolean isSuccess() {
-        return success;
-    }
-
-    public void setSuccess(boolean success) {
-        this.success = success;
-    }
-
-    public JsonResult() {
+    public JSONResult() {
         super();
     }
 
-    public JsonResult(T data, String message, boolean success) {
+    /**
+     * 自定义返回的结果
+     *
+     * @param data
+     * @param message
+     * @param success
+     */
+    public JSONResult(T data, String message, boolean success) {
         this.data = data;
-        this.message = message;
-        this.success = success;
+        super.setMessage(message);
+        super.setSuccess(success);
     }
 
-    public JsonResult(T data, String message) {
+    /**
+     * 成功返回数据和消息
+     *
+     * @param data
+     * @param message
+     */
+    public JSONResult(T data, String message) {
         this.data = data;
-        this.message = message;
-        this.success = true;
+        super.setMessage(message);
+        super.setSuccess(true);
     }
 
-    public JsonResult(T data) {
+    /**
+     * 成功返回数据
+     *
+     * @param data
+     */
+    public JSONResult(T data) {
         this.data = data;
-        this.success = true;
+        super.setSuccess(true);
     }
 }
