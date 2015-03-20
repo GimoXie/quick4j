a73348460f2aeea87b56d67591b3a4ad0dcacac9
tongyuan.zou <tongyuan.zou@shuyun.com>
2015-03-20 11:24:36 +0800

    添加jedis

--- src/test/java/com/eliteams/quick4j/test/library/jedis/JedisTest.java
+++ src/test/java/com/eliteams/quick4j/test/library/jedis/JedisTest.java
@@ -1,8 +1,10 @@
 package com.eliteams.quick4j.test.library.jedis;
 
+import com.eliteams.quick4j.core.feature.cache.redis.RedisCache;
 import com.eliteams.quick4j.core.feature.test.TestSupport;
 import org.junit.Test;
-import redis.clients.jedis.Jedis;
+
+import javax.annotation.Resource;
 
 /**
  * JedisTest : 测试 jedis 功能
@@ -12,18 +14,18 @@ import redis.clients.jedis.Jedis;
  */
 public class JedisTest extends TestSupport {
 
-    public int port = 6379;
-    public String host = "127.0.0.1";
-    public Jedis jedis = new Jedis(host, port);
+
+    @Resource
+    private RedisCache redisCache;
 
 
     @Test
     public void testSet() {
-        jedis.set("appID", System.currentTimeMillis() + "");
+        redisCache.cache("anchor", "StarZou", 1 * 60 * 24);
     }
 
     @Test
     public void testGet() {
-        System.out.printf(jedis.get("appID"));
+        System.out.printf(redisCache.get("anchor"));
     }
 }
